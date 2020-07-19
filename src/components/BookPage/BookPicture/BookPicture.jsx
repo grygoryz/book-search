dd82f202da0e7676fe8300a2e530e89f8e20bdf1
grygoryz <grigoryts@yahoo.com>
2020-07-19 15:12:25 +0300

    BookPicture animation

--- src/components/BookPage/BookPicture/BookPicture.jsx
+++ src/components/BookPage/BookPicture/BookPicture.jsx
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 import c from "./BookPicture.module.scss";
 import coverFallback from "../../../assets/book-cover-fallback-big.png";
+import {animated, useSpring, config} from "react-spring"
 
 const BookPicture = ({imageLink}) => {
     const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+    const [props, set] = useSpring(() => ({ transform: "scale(1)",  config: config.wobbly}));
 
     const placeholder = <svg width="300" height="420" viewBox="0 0 300 420">
         <rect width="300" height="420" rx="10" ry="10" fill="#CCC"/>
@@ -13,9 +16,15 @@ const BookPicture = ({imageLink}) => {
         <div className={c.container}>
             <div>
                 {!isImageLoaded ? placeholder : null}
-                <img style={!isImageLoaded ? {visibility: "hidden"} : {}}
-                     src={imageLink ? imageLink : coverFallback}
-                     onLoad={() => setIsImageLoaded(true)} alt=""/>
+                <animated.div onMouseEnter={() => set({transform: "scale(1.1)"})}
+                              onMouseLeave={() => set({transform: "scale(1)"})}
+                              className={c.pictureWrapper}
+                              style={props}>
+
+                    <img style={!isImageLoaded ? {visibility: "hidden"} : {}}
+                         src={imageLink ? imageLink : coverFallback}
+                         onLoad={() => setIsImageLoaded(true)} alt=""/>
+                </animated.div>
             </div>
         </div>
     );
